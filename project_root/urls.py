98e42ba2ca4b905f0eed5ca53eb80c7bb1e48b2f
brightmorkli37 <brightmorkli37@gmail.com>
2022-06-10 13:29:26 +0000

    static and media files config

--- project_root/urls.py
+++ project_root/urls.py
@@ -15,9 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = 'StoreBox Cloud'
+admin.site.site_title = 'StoreBox Cloud'
+admin.site.index_title = 'StoreBox | Administration'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("accounts.urls", namespace="accounts")),
     path('dashboard/', include("storage.urls", namespace="storage")),
 ]
+
+urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
+urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
--- storage/views.py
+++ storage/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def dashboard(request):
 
-    template_name = 'storage/dashboard.html'
+    template_name = 'storage/base.html'
     context = {}
     return render(request, template_name, context)
