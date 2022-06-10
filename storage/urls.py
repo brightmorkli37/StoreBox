9134e5e8913ef70b0f8319ae7bce7740b9089d6d
brightmorkli37 <brightmorkli37@gmail.com>
2022-06-10 16:14:51 +0000

    add pages

--- storage/urls.py
+++ storage/urls.py
@@ -6,4 +6,7 @@ app_name = "storage"
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('images/', views.upload_images, name='my_images'),
+    path('audio_files/', views.upload_audio, name='my_audios'),
+    path('videos/', views.upload_videos, name='my_videos'),
+    path('documents/', views.upload_files, name='my_files'),
 ]
--- storage/views.py
+++ storage/views.py
@@ -1,17 +1,38 @@
 from multiprocessing import context
-from re import template
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='accounts:login')
 def dashboard(request):
 
     template_name = 'storage/dashboard.html'
     context = {}
     return render(request, template_name, context)
 
-
+@login_required(login_url='accounts:login')
 def upload_images(request):
 
     template_name = 'storage/images.html'
+    context = {}
+    return render(request, template_name, context)
+
+@login_required(login_url='accounts:login')
+def upload_audio(request):
+    
+    template_name = 'storage/audio_files.html'
+    context = {}
+    return render(request, template_name, context)
+
+@login_required(login_url='accounts:login')
+def upload_videos(request):
+    
+    template_name = 'storage/videos.html'
+    context = {}
+    return render(request, template_name, context)
+
+def upload_files(request):
+    
+    template_name = 'storage/documents.html'
     context = {}
     return render(request, template_name, context)
