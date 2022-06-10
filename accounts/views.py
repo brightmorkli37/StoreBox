f06956291a3d67315269584210798262512b0b93
brightmorkli37 <brightmorkli37@gmail.com>
2022-06-10 11:48:44 +0000

    index page setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
+from multiprocessing import context
+from re import template
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+
+    template_name = 'accounts/index.html'
+    context = {}
+    return render(request, template_name, context)
